b8f4d638424e87081ed3f656b8b6435aa7fc9ead
Muhammad Maaz Arsalan Batla <maazarsalan@outlook.com>
2023-12-01 06:39:03 +0500

    Work on "Add Manually" feature completed.

--- MarkMate/MarkMate/Teacher/Controllers/TeacherViewController.swift
+++ MarkMate/MarkMate/Teacher/Controllers/TeacherViewController.swift
@@ -8,9 +8,11 @@
 import Foundation
 import UIKit
 
-import UIKit
+protocol AddStudentsViewControllerDelegate: AnyObject {
+    func didEnterData(name: String, erp: String)
+}
 
-class AddingStudentsViewController: UIViewController, UIDocumentPickerDelegate {
+class AddingStudentsViewController: UIViewController, UIDocumentPickerDelegate, AddStudentsViewControllerDelegate {
     
     @IBOutlet weak var ImportExcelButton: UIButton!
     
@@ -46,55 +48,96 @@ class AddingStudentsViewController: UIViewController, UIDocumentPickerDelegate {
     }
     
     var studentsData: [Student] = []
-    
+        
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "segueToAddStudent" {
             if let addStudentVC = segue.destination as? AddStudentViewController {
                 addStudentVC.studentsData = studentsData
+                addStudentVC.delegate = self
             }
         }
     }
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    func didEnterData(name: String, erp: String) {
+        let newStudent = Student(name: name, erp: erp)
+        studentsData.append(newStudent)
+        tableView.reloadData()
+    }
 }
-    class AddStudentViewController: UIViewController {
 
-        @IBOutlet weak var StudentName: UITextField!
-        @IBOutlet weak var StudentERP: UITextField!
+class StudentTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var erpLabel: UILabel!
 
-        var studentsData: [Student] = []
+    func configure(with student: Student) {
+        nameLabel.text = "\(student.name)"
+        erpLabel.text = "\(student.erp)"
+    }
+}
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-        }
+    // Adopting UITableViewDelegate and UITableViewDataSource
+extension AddingStudentsViewController: UITableViewDelegate, UITableViewDataSource {
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentsData.count
+    }
 
-        @IBAction func SaveButtonTapped(_ sender: UIButton) {
-            addStudent()
-            printStudentData()
-            dismiss(animated: true, completion: nil)
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studentCell", for: indexPath)  as! StudentTableViewCell
+        let student = studentsData[indexPath.row]
+        cell.configure(with: student)
+        return cell
+    }
+}
+
+class AddStudentViewController: UIViewController, AddStudentsViewControllerDelegate {
+    
+    func didEnterData(name: String, erp: String) {
+    }
+    
+
+    @IBOutlet weak var StudentName: UITextField!
+    @IBOutlet weak var StudentERP: UITextField!
+
+    weak var delegate: AddStudentsViewControllerDelegate?
+    
+    var studentsData: [Student] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func SaveButtonTapped(_ sender: UIButton) {
+        addStudent()
+        printStudentData()
+        dismiss(animated: true, completion: nil)
+    }
         
-        @IBAction func AddAnotherButtonTapped(_ sender: UIButton){
-            addStudent()
-            clearTextFields()
-        }
+    @IBAction func AddAnotherButtonTapped(_ sender: UIButton){
+        addStudent()
+        clearTextFields()
+    }
 
-        private func addStudent() {
-            guard let name = StudentName.text, let erp = StudentERP.text else {
-                return
-            }
-            let student = Student(name: name, erp: erp)
-            studentsData.append(student)
+    private func addStudent() {
+        guard let name = StudentName.text, let erp = StudentERP.text else {
+            return
         }
+        let student = Student(name: name, erp: erp)
+        delegate?.didEnterData(name: name, erp: erp)
+        studentsData.append(student)
+    }
 
-        private func clearTextFields() {
-            StudentName.text = ""
-            StudentERP.text = ""
-        }
+    private func clearTextFields() {
+        StudentName.text = ""
+        StudentERP.text = ""
+    }
         
-        private func printStudentData() {
-            print("Student Data:")
-            for student in studentsData {
-                print("Name: \(student.name), ERP: \(student.erp)")
-            }
+    private func printStudentData() {
+        print("Student Data:")
+        for student in studentsData {
+            print("Name: \(student.name), ERP: \(student.erp)")
         }
+    }
 }
